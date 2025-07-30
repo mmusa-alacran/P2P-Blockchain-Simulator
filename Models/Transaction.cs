5ed2e3cf0368a3c02e99320072be4e75a437fb96
Momin Musa <straighty14.khan@gmail.com>
2025-07-30 21:17:32 +0200

    Transaction: Transaction.cs

---
+++ Models/Transaction.cs
@@ -0,0 +1,7 @@
+namespace CsharpBlockchainNode.Models;
+namespace CsharpBlockchainNode.Models;
+
+
+/// Represents a transaction using a record type,
+/// which provides immutable properties and value-based equality.
+public record Transaction(string From, string To, double Amount);
