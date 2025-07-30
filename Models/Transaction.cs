dd48f4fbcd2c62bfad73423e2620cbb19db04b3c
Momin Musa <straighty14.khan@gmail.com>
2025-07-30 21:34:31 +0200

    fixes

--- Models/Transaction.cs
+++ Models/Transaction.cs
@@ -1,7 +1,6 @@
-namespace CsharpBlockchainNode.Models;
-namespace CsharpBlockchainNode.Models;
-
-
+namespace CsharpBlockchainNode.Models
+{
 /// Represents a transaction using a record type,
 /// which provides immutable properties and value-based equality.
 public record Transaction(string From, string To, double Amount);
+}
