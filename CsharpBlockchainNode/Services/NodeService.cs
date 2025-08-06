e49d77e939dbf4e22872dfb070bcdb271cafae08
Momin Musa <straighty14.khan@gmail.com>
2025-08-06 17:15:33 +0200

    file added: NodeService.cs

---
+++ CsharpBlockchainNode/Services/NodeService.cs
@@ -0,0 +1,10 @@
+namespace CsharpBlockchainNode.Services;
+
+
+/// This service will handle all peer-to-peer (P2P) networking logic,
+/// such as connecting to other nodes, broadcasting data, and syncing the chain.
+
+public class NodeService
+{
+    // add P2P logic here later.
+}
